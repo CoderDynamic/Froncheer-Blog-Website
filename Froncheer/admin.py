55101e6974f90d3c201db3b6d67c163fd77f0340
Avishek <abhishekbhattacharjee67@gmail.com>
2022-03-26 23:36:48 +0530

    Initial Stage

---
+++ Froncheer/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from Froncheer.models import Topic
+from .models import Topic
+
+# Register your models here.
+admin.site.register(Topic)
---
+++ Froncheer/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Topic
+# Create your views here.
+def index(request):
+    topics = Topic.objects.all()
+    
+    return render(request, "index.html", {'topics' :topics })
+    
